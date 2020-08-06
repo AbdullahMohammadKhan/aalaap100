0313d2ca2d3ba9b0c08e83780a0f1f8b6a74d4bd
abdullah <hbkamk@gmail.com>
2020-08-06 23:41:48 +0600

    Updates

---
+++ precache-manifest.5daa4243d681878a2bca77f26ea8e11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36719da82f89a2656b1a29f93a37cab",
+    "url": "/aalaap100/index.html"
+  },
+  {
+    "revision": "74cba716509e77cadbf5",
+    "url": "/aalaap100/static/css/main.b9b6581f.chunk.css"
+  },
+  {
+    "revision": "41cf5ed94f3bfc34f6e9",
+    "url": "/aalaap100/static/js/2.0ab0b65c.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/aalaap100/static/js/2.0ab0b65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74cba716509e77cadbf5",
+    "url": "/aalaap100/static/js/main.439d182c.chunk.js"
+  },
+  {
+    "revision": "5916d29ee7e7ba859c61",
+    "url": "/aalaap100/static/js/runtime-main.d455a856.js"
+  }
+]);
